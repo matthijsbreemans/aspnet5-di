b58c4e6af6cfc3f3d15c99b6f1de33707552358c
Matthijs Breemans <matthijs>
2016-03-18 23:32:58 +0100

    powershell build script

---
+++ build.ps1
@@ -0,0 +1,12 @@
+﻿
+$DNU = "dnu"
+$DNVM = "dnvm"
+
+# ensure the correct version
+& $DNVM install 1.0.0-rc1-update1
+
+# use the correct version
+& $DNVM use 1.0.0-rc1-update1
+
+& $DNU restore "$ProjectJsonPath"
+& $DNU build "$ProjectJsonPath"
